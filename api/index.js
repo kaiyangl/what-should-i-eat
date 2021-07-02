b1259a2cc65163505e9c0bab8be1bb3d3c4b2a17
Kai Yang Lim <lim_kaiyang@hotmail.com>
2021-07-02 22:37:23 +1000

    Initial API and webpage

---
+++ api/index.js
@@ -0,0 +1,70 @@
+const fetch = require("node-fetch");
+
+const databaseId = process.env.NOTION_DATABASE_ID;
+const apiKey = process.env.NOTION_API_KEY;
+
+function parseResponse(response, data) {
+  response.results.forEach((result) => {
+    const { MainTags, Vegetarian, Type, Name } = result.properties;
+
+    data.push({
+      name: Name.title[0].text.content,
+      vegetarian: Vegetarian.checkbox,
+      tags: MainTags.multi_select.map((i) => i.name),
+      type: Type.multi_select.map((i) => i.name),
+    });
+  });
+}
+
+async function recursivelyGetData(id, cursor, data) {
+  const body = {
+    filter: {
+      property: "Name",
+      text: {
+        is_not_empty: true,
+      },
+    },
+  };
+
+  if (cursor) {
+    body.start_cursor = cursor;
+  }
+
+  const response = await fetch(
+    `https://api.notion.com/v1/databases/${id}/query`,
+    {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        "Notion-Version": "2021-05-13",
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        filter: {
+          property: "Name",
+          text: {
+            is_not_empty: true,
+          },
+        },
+        start_cursor: cursor,
+      }),
+    }
+  );
+
+  const jsonData = await response.json();
+
+  if (jsonData.has_more) {
+    console.log(jsonData.next_cursor);
+    await recursivelyGetData(databaseId, jsonData.next_cursor, data);
+  }
+
+  parseResponse(jsonData, data);
+}
+
+module.exports = (req, res) => {
+  const data = [];
+  await recursivelyGetData(databaseId, undefined, data);
+
+  res.json(data);
+};
